d74e8eec1187e3caf157e0e43904ffb36b2eeeb1
lizhihui <1535924940@qq.com>
2017-06-16 18:17:50 +0800

    eureka完成

---
+++ eureka/src/main/java/eureka/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by lizhihui on 2017-06-16 12:31.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
